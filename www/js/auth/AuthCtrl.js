fe3ede09c0a3511e40a886369fe471138e5cde8e
Conor Flannigan <cflanni1@jhu.edu>
2015-04-06 16:53:36 -0700

    (feat) Implement login/signup page
    Includes custom CSS and docco comments

--- www/js/auth/AuthCtrl.js
+++ www/js/auth/AuthCtrl.js
@@ -3,13 +3,18 @@ angular.module('snapcache.auth', [])
 
 .controller('AuthCtrl', function($scope) {
 
+  // We'll want to know when the user wants to login
+  // or is here to sign up.
   $scope.isLogin = true;
 
+  // We'll also want to save their data somewhere...
   $scope.loginData = {
     username: '',
     password: ''
   };
 
+  // `submit` decides whether to call `login` or `submit`
+  // depending on the status of the user.
   $scope.submit = function() {
     if ($scope.isLogin) {
       $scope.login();
@@ -18,12 +23,19 @@ angular.module('snapcache.auth', [])
     }
   }
 
+  // Use Authentication service to login user
   $scope.login = function() {
     console.log('Logging in');
   };
 
+  // Use Authentication service to sign user up
   $scope.signup = function() {
     console.log('Signing up');
+  };
+
+  // Toggles status of user (new vs. returning)
+  $scope.toggleLogin = function() {
+    $scope.isLogin = !$scope.isLogin;
   };
 
 });
