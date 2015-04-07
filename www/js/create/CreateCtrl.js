fb288121c8089bfef8ec29cc1462a1cd331dc8eb
Conor Flannigan <cflanni1@jhu.edu>
2015-04-07 15:50:54 -0700

    (fix) Update create modal

--- www/js/create/CreateCtrl.js
+++ www/js/create/CreateCtrl.js
@@ -1,7 +1,7 @@
 // Create Controller
 angular.module('snapcache.create', [])
 
-.controller('CreateCtrl', function() {
+.controller('CreateCtrl', function($ionicModal) {
 
   var self = this;
 
@@ -11,8 +11,22 @@ angular.module('snapcache.create', [])
     console.log('New cache submitted');
   };
 
-  self.getLocation = function() {
-    // TODO: get location from map modal
-  }
+  // Create the map modal that we will use later
+  $ionicModal.fromTemplateUrl('js/create/map.html', {
+    scope: self
+  }).then(function(modal) {
+    self.modal = modal;
+  });
+
+  // Triggered in the map modal to close it
+  self.closeMap = function() {
+    self.modal.hide();
+  };
+
+  // Open the map modal
+  self.map = function() {
+    console.log('getting location');
+    self.modal.show();
+  };
 
 });
