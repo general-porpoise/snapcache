de1360cd6056fc2540409489a7a3d72c26b4231f
annekef <anneke.floor@gmail.com>
2015-04-10 17:29:52 -0700

    (feat) Create basic outbox skeleton
    - Add in controller
    - Add in view/html file
    - Add script include in index.html
    - Add outbox module declaration to snapcache.js

---
+++ www/js/outbox/OutboxCtrl.js
@@ -0,0 +1,13 @@
+// Outbox module
+angular.module('snapcache.outbox', [])
+
+.controller('OutboxCtrl', function () {
+  var self = this;
+  self.caches = {};
+
+  self.displayCaches = function () {
+  };
+
+  // self.displayCaches();
+});
+
