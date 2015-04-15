a97a72107255b0555660989597d15519cbe6e295
Chris Rinaldi <cgrinaldi@gmail.com>
2015-04-15 15:29:12 -0700

    (feat) Display all contents of a particular cache in outbox detail

--- www/js/detail/OutboxDetailCtrl.js
+++ www/js/detail/OutboxDetailCtrl.js
@@ -5,4 +5,19 @@ angular.module('snapcache.detail.outbox', [])
 .controller('OutboxDetailCtrl', function (userSession) {
   var self = this;
   self.cache = userSession.currentCache;
+  self.texts = [];
+
+  // Load the cache's text objects into an array
+  // NOTE: Currently, the assumed object structure is the following:
+  //  - CacheID
+  //    - contents
+  //      - text
+  //        - ContributionID
+  //          - contributor
+  //          - message
+  var texts = self.cache.contents.text;
+  for (var id in texts) {
+    self.texts.push(texts[id]);
+  }
+
 });
