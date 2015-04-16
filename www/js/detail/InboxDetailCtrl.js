3a8822c2b2b43921f7853cf63191d135f435b4da
Chris Rinaldi <cgrinaldi@gmail.com>
2015-04-15 21:40:36 -0700

    (feat) Display all the contents of a cache in inboxDetail

--- www/js/detail/InboxDetailCtrl.js
+++ www/js/detail/InboxDetailCtrl.js
@@ -5,4 +5,11 @@ angular.module('snapcache.detail.inbox', [])
 .controller('InboxDetailCtrl', function (userSession) {
   var self = this;
   self.cache = userSession.currentCache;
+  self.texts = [];
+
+  // Load the cache's text objects into an array
+  var texts = self.cache.contents.text;
+  for (var id in texts) {
+    self.texts.push(texts[id]);
+  }
 });
