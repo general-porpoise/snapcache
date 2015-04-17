a9344bcf6d68ebbf8e79554ff90078200badafd3
Chris Rinaldi <cgrinaldi@gmail.com>
2015-04-16 17:07:49 -0700

    (refactor) Remove getLocation and associated rate-limiting timers
    Since we are moving getLocation to a service (and one that does not
    have to be constantly updated), we no longer need it in the menuCtrl.

--- www/js/sidemenu/MenuCtrl.js
+++ www/js/sidemenu/MenuCtrl.js
@@ -1,7 +1,7 @@
 // Menu Controller
 angular.module('snapcache.menu', [])
 
-.controller('MenuCtrl', function(FIREBASE_REF, Caches, $scope, $ionicModal, $ionicPlatform, userSession, Geofire) {
+.controller('MenuCtrl', function(FIREBASE_REF, Caches, $scope, $ionicModal, $ionicPlatform, userSession, Geofire, Location) {
 
   var self = this;
   self.position;
@@ -128,12 +128,7 @@ angular.module('snapcache.menu', [])
       console.log('current position:', pos);
       userSession.position = pos;
       self.position = pos;
-      console.log(userSession.uid);
-      // get human-readable location (address), throttled to 1
-      // request per 20 seconds and only runs app when in foreground
-      if (inForeground && Date.now() > self.geocodingTimeout) {
-        self.getAddress();
-      }
+
       // remove key in order to satisfy the 'key entered' event
       // (when key already inside radius)
       Geofire.geofire.remove(userSession.uid);
@@ -157,37 +152,4 @@ angular.module('snapcache.menu', [])
     // remove user from geofire
     Geofire.geofire.remove(userSession.uid);
   };
-
-  // Timeout used to throttle geocoding requests
-  self.geocodingTimeout = Date.now();
-
-  // Get reverse geocoding from lat/lng coords for
-  // human-readable location
-  self.getAddress = function() {
-    console.log('Requesting human-readable location');
-    var geocoder = new google.maps.Geocoder();
-    var lat = userSession.position.coords.latitude;
-    var lng = userSession.position.coords.longitude;
-    var latlng = new google.maps.LatLng(lat, lng);
-    // request reverse geocode from geocoder
-    geocoder.geocode({'latLng': latlng}, function(results, status) {
-      if (status == google.maps.GeocoderStatus.OK) {
-        self.geocodingTimeout = Date.now() + 20000;
-        if (results[0]) {
-          // store the human-readable
-          self.readable_location = results[0].formatted_address;
-          userSession.readable_location = self.readable_location;
-        } else {
-          console.log('No human-readable location found');
-        }
-      } else {
-        // If we receive an error status, delay future geocoding request
-        self.geocodingTimeout = Date.now() + 40000;
-        self.readable_location = undefined;
-        userSession.readable_location = undefined;
-        alert('Geocoder failed due to: ' + status);
-      }
-    });
-  };
-
 });
