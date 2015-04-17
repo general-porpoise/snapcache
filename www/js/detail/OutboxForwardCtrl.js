35e7c4df3d65e114fdc0d32e3c318dcee77aee78
Chris Rinaldi <cgrinaldi@gmail.com>
2015-04-17 14:57:55 -0700

    (feat) Add/remove friend from contributors list using `toggleContributor()`

--- www/js/detail/OutboxForwardCtrl.js
+++ www/js/detail/OutboxForwardCtrl.js
@@ -3,12 +3,29 @@ angular.module('snapcache.outbox.forward', [])
 
 .controller('OutboxForwardCtrl', function($scope, UserFriends){
 
+  // Set default values
   $scope.person = {};
   $scope.person.name = '';
+  $scope.contributors = {};
 
+  // `search()` is used to display/filter potential people the user
+  // can invite to contribute.
   $scope.search = function() {
     $scope.potentialContributors = UserFriends.search($scope.person.name);
   };
 
+  // `toggleContributor()` will add/remove a friend from the list of contributors
+  // based on if the friend is in the contributors obj or not.
+  $scope.toggleContributor = function(friend) {
+    var id = friend.id;
+    if (id in $scope.contributors) {
+      delete $scope.contributors[id];
+    } else {
+      $scope.contributors[id] = friend;
+    }
+    console.log('contributors are', $scope.contributors);
+  };
+
+  // Initialize search results to display all friends.
   $scope.search();
 });
