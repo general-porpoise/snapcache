46c8eb1eb070293b4c9faa2e8bc6752ef1c56553
Conor Flannigan <cflanni1@jhu.edu>
2015-04-18 15:29:43 -0700

    (fix) Fix bug where new caches were duplicated in inbox/outbox

--- www/js/shared/GeofireService.js
+++ www/js/shared/GeofireService.js
@@ -67,6 +67,7 @@ angular.module('snapcache.services.geofire', [])
   var setListener = function(cacheID) {
     console.log('in setListener');
     cachesRef.child(cacheID).once('value', function(snapshot) {
+      console.log('Creating geoquery');
       var cacheObj = snapshot.val();
       var geoQuery = geofire.query({
         center: [cacheObj.coordinates.latitude, cacheObj.coordinates.longitude],
@@ -82,10 +83,6 @@ angular.module('snapcache.services.geofire', [])
           var now = Date.now();
           var droptime = cacheObj.droptime;
           var lifespan = cacheObj.lifespan;
-          console.log('now:', now);
-          console.log('droptime:', droptime);
-          console.log('droptime+lifespan:', droptime+lifespan);
-          console.log('discovered:', isDiscovered);
           // check if cache is available to be discovered
           if ((now > droptime) &&
              (now < (droptime + lifespan)) &&
@@ -97,12 +94,14 @@ angular.module('snapcache.services.geofire', [])
         });
       });
       // remove geoquery listener when cache removed
+      console.log('Setting removal listener');
       cachesRef.on('child_removed', function(childSnapshot) {
         if (childSnapshot.key() === cacheID) {
+          console.log('Cache removed, cancelling geoquery');
           geoQuery.cancel();
         }
       });
-    })
+    });
   };
 
   return {
